d6eb36bfb41fe748e8182b72700a9d82d14a2dc7
A-Quaglia <augustoquaglia@outlook.com>
2023-01-29 00:32:15 -0300

    refactor: change methods and columns names refactor: make_sumary method

--- paretoabc/pareto.py
+++ paretoabc/pareto.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 class CurveABC:
 
-      def __init__(self, df: pd.DataFrame, abc_column: str, label_a: float = 0.2,
-                 label_b: float = 0.5, label_c: float = 0.3):
+    def __init__(self, df: pd.DataFrame, abc_column: str, label_a: float = 0.2,
+                 label_b: float = 0.5):
 
         self.df = df.copy()
         self.abc_column = abc_column
@@ -16,14 +16,14 @@ class CurveABC:
         self.total_items = 0
         self.total_values = 0
         self.items_rank = pd.Series([0])
-        self.item_porc = pd.Series([0])
-        self.values_porc = pd.Series([0])
-        self.values_cum_porc = pd.Series([0])
+        self.item_perc = pd.Series([0])
+        self.values_perc = pd.Series([0])
+        self.values_cum_perc = pd.Series([0])
         self.labels = pd.Series([0])
 
         self.label_a = label_a
         self.label_b = label_b
-        self.label_c = label_c
+        self.label_c = 1 - (label_a + label_b)
 
         self.abc_columns_names = {"rank": "rank", "item_porc": "items representation",
                                   "values_porc": "values representation", "label": 'abc_labels'}
@@ -38,16 +38,16 @@ class CurveABC:
         self.items_rank = self.values.rank(method=method, ascending=ascending)
         return self.items_rank
 
-    def _make_item_porc(self):
+    def _make_item_perc(self):
         self.total_items = self.items_rank.count()
-        self.item_porc = self.items_rank / self.total_items
-        return self.item_porc
+        self.item_perc = self.items_rank / self.total_items
+        return self.item_perc
 
-    def _make_values_porc(self):
+    def _make_values_perc(self):
         self.total_values = self.values.sum()
-        self.values_porc = self.values / self.total_values
-        self.values_cum_porc = self.values_porc.cumsum()
-        return self.values_cum_porc
+        self.values_perc = self.values / self.total_values
+        self.values_cum_perc = self.values_perc.cumsum()
+        return self.values_cum_perc
 
     def _classify_abc(self, value):
         if value <= self.label_a:
@@ -58,20 +58,19 @@ class CurveABC:
             return "C"
 
     def _make_labels(self):
-        self.labels = self.item_porc.apply(self._classify_abc)
+        self.labels = self.item_perc.apply(self._classify_abc)
         return self.labels
 
-    def abc(self, automatic=True):
-        if automatic:
-            self._make_rank()
-            self._make_item_porc()
-            self._make_values_porc()
-            self._make_labels()
+    def abc(self):
+        self._make_rank()
+        self._make_item_perc()
+        self._make_values_perc()
+        self._make_labels()
 
         cols = self.abc_columns_names
         self.df[cols["rank"]] = self.items_rank
-        self.df[cols["item_porc"]] = self.item_porc
-        self.df[cols["values_porc"]] = self.values_cum_porc
+        self.df[cols["item_perc"]] = self.item_perc
+        self.df[cols["values_perc"]] = self.values_cum_perc
         self.df[cols["label"]] = self.labels
 
         self.make_summary()
@@ -98,26 +97,22 @@ class CurveABC:
         return self.df[self.df[self.abc_columns_names["label"]] == label].last_valid_index()
 
     def _get_item_rep(self, pos: int):
-        return self.df.loc[pos, self.abc_columns_names["item_porc"]]
+        return self.df.loc[pos, self.abc_columns_names["item_perc"]]
 
     def _get_value_rep(self, pos: int):
-        return self.df.loc[pos, self.abc_columns_names["values_porc"]]
+        return self.df.loc[pos, self.abc_columns_names["values_perc"]]
 
     def make_summary(self):
         last_a = self._get_last_label("A")
         last_b = self._get_last_label("B")
         last_c = self._get_last_label("C")
 
-        table = {"Labels": ["A", "B", "C"], self.abc_columns_names["item_porc"]: [],
-                 self.abc_columns_names["values_porc"]: []}
-
-        table[self.abc_columns_names["item_porc"]] = [self._get_item_rep(last_a),
-                                                      self._get_item_rep(last_b),
-                                                      self._get_item_rep(last_c)]
-
-        table[self.abc_columns_names["values_porc"]] = [self._get_value_rep(last_a),
-                                                        self._get_value_rep(last_b),
-                                                        self._get_value_rep(last_c)]
+        table = {"Labels": ["A", "B", "C"], self.abc_columns_names["item_perc"]: [self._get_item_rep(last_a),
+                                                                                  self._get_item_rep(last_b),
+                                                                                  self._get_item_rep(last_c)],
+                 self.abc_columns_names["values_perc"]: [self._get_value_rep(last_a),
+                                                         self._get_value_rep(last_b),
+                                                         self._get_value_rep(last_c)]}
 
         self.abc_summary_table = pd.DataFrame(table)
         return self.abc_summary_table
